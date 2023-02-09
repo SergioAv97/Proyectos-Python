31f413123cf96d2c8773c52412cb34f8c35d8d9e
SergioAv97 <sacdev97@gmail.com>
2023-02-09 15:09:15 +0100

    Bug no finish bucle

--- Dados.py
+++ Dados.py
@@ -9,11 +9,29 @@ import random
 
 def dados ():
     caras = [1,2,3,4,5,6]
-
     caraRandom = random.randint(0,5)
+    repetimos = False
+    vidas = 3
     print('El dado ha sido lanzado: "Que comience el Juego"')
     print("Dispones de 3 Vidas es la regla de todos los juegos mala suerte")
     print("Cada fallo te restara una vida, a continuacion se te dara una pista")
-    print(caras[caraRandom])
+    print("===================================================================")
+    while repetimos == False:
 
+        print("Introduzca el numero que piensa que ha salido:")
+        numeroUser = int(input())
+        print("numero introducido =", numeroUser )
+        print('numero dado', caraRandom)
+        if numeroUser == caraRandom:
+            print('MUY BIEN HAS GANADO A LA MAQUINA SOBREVIVES')
+            repetimos == True
+        elif(numeroUser != caraRandom):
+            if vidas != 0:
+                vidas = vidas -1
+                print('Vidas:',vidas)
+                print('Otro Intento')
+        elif vidas == 0:
+            print('La Maquina gana y tu MUERES')
+
+    print('salida del bucle')
 dados()
